fb7b5e5c4d1cd907847e61c868696ea8b8f6ad56
jjzapata81 <jjzapata81@hotmail.com>
2019-10-23 01:00:31 -0500

    Se suben imagenes

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import logoUco from '../logouco.png';
+import logoTech from '../logomain.png';
 
 function MainContent(){
     return(
         <div>
-            <Container className="main-content-text justify-content-center">
-                <Row>2da Hackaton</Row>
-                <Row>UCO - Tech and Solve</Row>
+            <Container className="main-content-text">
+                <Row className="justify-content-center">2da Hackaton</Row>
+                <Row className="justify-content-center">UCO - Tech and Solve</Row>
+                <Row>
+                    <Col className="justify-content-center"><img src={logoUco} className="imagen"/></Col>
+                    <Col className="justify-content-center"><img src={logoTech} className="imagen"/></Col>
+                </Row>
             </Container>
 
         </div>
